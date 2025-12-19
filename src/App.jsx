674da89d42c59b0415a4759296f510acda430d7f
Thami <sainttsquared@gmail.com>
2025-12-19 12:18:59 +0200

    modified profile

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import Footer from "./components/footer/Footer";
 import AboutSection from "./components/about/AboutSection";
-import ContactSection from "./components/Contact/ContactSection";
+import ContactSection from "./components/contact/ContactSection";
 import ProjectsSection from "./components/projects/ProjectSection";
 import SkillsSection from "./components/skills/SkillsSection";
 import AOS from "aos";
