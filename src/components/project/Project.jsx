dff87a89a529d4a71839d66fcff2d364350885d7
Thami <sainttsquared@gmail.com>
2025-12-31 13:34:25 +0200

    temp remove video add placeholder

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,20 +1,11 @@
+import { MdOutlineConstruction } from "react-icons/md";
 import "./project-card.css";
 import { useRef } from "react";
+import { LuHardHat } from "react-icons/lu";
+import { BiCheck } from "react-icons/bi";
 
-function Project({ projectInfo: data }) {
-  const { name, stack } = data;
+function Project({ name, features, desc, stack }) {
 
-  const projectInfo = Object.entries(stack).map((s, index) => {
-    const [name, icon] = s;
-    return (
-      <div key={index} className="project-info">
-        {icon}
-        <span>{name}</span>
-      </div>
-    );
-  });
-
-  // Create a ref to attach to the video element
   const videoRef = useRef(null);
 
   const handleMouseEnter = () => {
@@ -26,34 +17,45 @@ function Project({ projectInfo: data }) {
   const handleMouseLeave = () => {
     if (videoRef.current) {
       videoRef.current.pause();
-      // Optional: Reset video to the beginning
       videoRef.current.currentTime = 0;
     }
   };
+console.log(stack);
 
   return (
     <div className="project-card" data-aos="zoom-in">
       <div className="project-video-container" >
-        <video className="project-video"
+        {/* <video className="project-video"
         ref={videoRef}
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
           muted
           loop
           preload="metadata">
-          <source src="/videos/default.mp4" type="video/mp4"></source></video>
+          <source src="/videos/default.mp4" type="video/mp4"></source></video> */}
+        <div className="project-video">
+          <p >Coming Soon!</p>
+          <div>
+            <MdOutlineConstruction /><LuHardHat />
+          </div>
+          <p className="small-text">under construction (let me cook)</p>
+        </div>
       </div>
       <div className="project-text-container">
-        <h2 className="project-title">Nextjs E-commerce</h2>
-        <p className="project-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat, praesentium suscipit doloremque perferendis, deleniti possimus officia ipsum animi aperiam perspiciatis sed id vel? Quod, suscipit officiis fugiat amet cumque quidem.</p>
+        <h2 className="project-title">{name}</h2>
         <h3 className="project-features">Key Features</h3>
-        <ul>
-          <li>Authentication</li>
-          <li>Cart Logic</li>
-          <li>Admin Dashboard</li>
-          <li>Integrated Payment gateway</li>
-          <li></li>
+        <ul className="feature-check">
+          {features.map((feature, index) => (
+            <li key={index}> <BiCheck /> {feature}</li>
+          ))}
+
         </ul>
+        <div>
+          <h2 className="tech-stack-title">Tech Stack</h2>
+          <ul className="tech-stack">
+            {stack.icons.map((icon, index) => (<li className="icon-size" key={index}>{icon}</li>))}
+          </ul>
+        </div>
       </div>
     </div>
   );
